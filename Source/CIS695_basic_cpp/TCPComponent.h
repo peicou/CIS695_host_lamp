fe5da0a6a0e4e659844e23344843db2bdea7504a
peicou <psandoval@gmail.com>
2016-04-24 19:20:40 -0400

    added comments to TCPComponent

--- Source/CIS695_basic_cpp/TCPComponent.h
+++ Source/CIS695_basic_cpp/TCPComponent.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/* Author: Francisco Sandoval*/
 
 #pragma once
 
@@ -12,12 +12,21 @@ class CIS695_BASIC_CPP_API UTCPComponent : public UActorComponent
 {
 	GENERATED_BODY()
 private:
+	//pointer to UE4 socket object
 	FSocket* SocketAsClient;
+	//stores compete ip v4 address
 	FIPv4Address ip;
 
 public:
+	//UPROPERTY:
+	//EditAnywhere - property can be edited by property windows in editor
+	//BlueprintReadWrite - can be modified in editor
+	//Category - category shown in editor
+	//address - ipaddress
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TCP Connection")
 		FString address;
+	
+	//port - port for ip address
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TCP Connection")
 		int32 port;
 
@@ -30,20 +39,30 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	//UFUNCTION:
+	//BlueprintCallable - can be called from the blueprint editor
+	//Category - category shown in editor
+	//tryTCPSocket - tries to connect using information in UPROPERTY
 	UFUNCTION(BlueprintCallable, Category = "TCP Connection")
 		void tryTCPSocket();
-
+	
+	//sendMsg - sends a message through socket connection
+	//str - string containing the message. The message is sent as a uchar array anyway, to be parsed at reception
 	UFUNCTION(BlueprintCallable, Category = "TCP Connection")
 		int32 sendMsg(const FString str);
 
+	//endConnection - terminates connection
 	UFUNCTION(BlueprintCallable, Category = "TCP Connection")
 		void endConnection();
 
+	//debugMsg - prints a debug message to the (host) screen
+	//str - message to be printed
 	void debugMsg(const FString str);
 
+	//method used internally to calculate UPROPERTY tagged values on editor changes
 	void CalculateValues();
 
+	//calls CalculateValues when a change is detected in-editor
 	void PostEditChangeProperty(FPropertyChangedEvent & PropertyChangedEvent);
-
 
 };
